7eea8b235861104f9ee1f26df03713fa7590affe
Ghassen Hamrouni <ghamrouni@iptechinside.com>
2012-01-04 11:50:21 +0100

    adding source code

---
+++ src/Recommender.h
@@ -0,0 +1,126 @@
+/*-
+* Copyright (c) 2012 Hamrouni Ghassen.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+* 1. Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in the
+*    documentation and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+* OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+* OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+* SUCH DAMAGE.
+*/
+
+/*
+ *
+ * Recommender system using matrix factorization (MF)
+ * Computing the product recommendation using latent factor models
+ *
+ */
+
+/*----------------------------------------------------------------------------------------------
+ *
+ * 									Learning algorithms
+ *
+ *----------------------------------------------------------------------------------------------
+ */
+
+struct training_set
+{
+	double** 		ratings;			//Known ratings
+	unsigned int 	training_set_size;  //The number of ratings
+};
+
+struct model_parameters
+{
+	unsigned int 	users_number;
+	unsigned int 	items_number;
+
+	double			lambda;			//The constant lambda controls the extent of regularization
+	double			step;			//step size in stochastic gradient descent algorithm
+
+	unsigned int	dimensionality; //dimensionality of the joint latent factor space
+	unsigned int	iteration_number;
+};
+
+struct learned_factors
+{
+	double** 		user_factor_vectors;
+	double** 		item_factor_vectors;
+};
+
+/*
+ * learn:			Learn using training set and the model parameters
+ * 							  
+ *
+ * Arguments:
+ *      tset		The training set contains the known rating of items
+ *      params		Parameters of the model
+ *
+ * Returns:
+ *      Return the learned factors.
+ *
+ */
+struct learned_factors*
+learn(struct training_set* tset, struct model_parameters* params);
+
+
+/*----------------------------------------------------------------------------------------------
+ *
+ * 									Rating estimation/computaion
+ *
+ *----------------------------------------------------------------------------------------------
+ */
+
+/*
+* rate_item:  Return the approximates user’s rating of an item
+*
+* Arguments:
+*      user_vector     The user's vector, measure the extent to which a user
+*      				   is interested in a factor
+*      item_vector     The item's vector, measure the extent to which the item
+*      				   possesses those factors
+*      dim   		   The size of the vectors
+*
+* Returns:
+*      The dot product between the two vectors.
+*
+*/
+double
+estimate_item_rating(double* user_vector, double* item_vector, unsigned int dim);
+
+/*
+* regularized_squared_error:  Return the the regularized squared
+* 							  error on the set of known ratings
+*
+* Arguments:
+*      user_vector     The user's factor vector to be computed
+*      item_vector     The item's factor vector to be computed
+*      r			   The known rating from the training set.
+*      lambda     	   The constant lambda controls the extent of regularization
+*      size   		   The size of the vectors
+*
+* Returns:
+*      Return the regularized squared error.
+*
+*/
+double
+regularized_squared_error(
+						  double* user_vector,
+						  double* item_vector,
+						  double r,
+						  double lambda,
+						  unsigned int size);
