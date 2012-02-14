ac6b48dfbf884ff614011ceb9f5e770acdef6b7d
Ghassen Hamrouni <ghamrouni@iptechinside.com>
2012-02-14 10:43:57 +0100

    Add clock counters in tests

--- test_suite/test_all.c
+++ test_suite/test_all.c
@@ -2,10 +2,15 @@
 #include "test_sparse_matrix_1.h"
 
 #include <assert.h>
+#include <stdio.h>
+#include <time.h>
 
 int main(void)
 {
 	int return_value;
+	clock_t start_c, end_c;
+
+	start_c = clock();
 
 	return_value = test_red_black_tree_1();
 
@@ -14,6 +19,9 @@ int main(void)
 	return_value = test_sparse_matrix_1();
 
 	assert(!return_value);
+
+	end_c = clock();
+	printf("Time : %f s \n", (double)(end_c - start_c)/ CLOCKS_PER_SEC);
 
 	return 0;
 }
